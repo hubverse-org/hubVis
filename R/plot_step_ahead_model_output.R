7b59692fb8e58be1b84c9542ada201fb9e0232bb
Lucie Contamin <CONTAMIN@pitt.edu>
2025-12-04 13:18:54 -0500

    Update R/plot_step_ahead_model_output.R
    Co-authored-by: Anna Krystalli <annakrystalli@googlemail.com>

--- R/plot_step_ahead_model_output.R
+++ R/plot_step_ahead_model_output.R
@@ -12,7 +12,7 @@
 #'@param use_median_as_point a `Boolean` for using median quantile as point
 #' in plot. Default to FALSE. If TRUE, will select first any `median`
 #' output type value and if no `median` value included in `model_out_tbl`;
-#' will select `quantile = 0.5` output type value. If no `median` or `quantile`,
+#' will select `quantile = 0.5` output type value. If no `median` or `quantile` value is included,
 #' will used the `sample` output type to calculate the median.
 #'@param intervals a vector of `numeric` values indicating which central
 #' prediction interval levels to plot.Value possibles:
